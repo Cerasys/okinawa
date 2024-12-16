786324f1a13a10ce1a16f1bdae03a8bedb9f4935
Nathan Lee <nathanblee@gmail.com>
2024-12-15 21:48:33 -0800

    correct date

--- src/components/MessageDisplay.jsx
+++ src/components/MessageDisplay.jsx
@@ -6,7 +6,7 @@ const MessageDisplay = ({ codeAfter }) => {
   const currentDate = new Date();
 
   // Convert the target date to the local time of the user's browser
-  const targetDate = new Date("2024-12-15T21:43:00-08:00"); // 9pm JST on Dec 25th, 2024
+  const targetDate = new Date("2024-12-25T21:00:00+09:00"); // 9pm JST on Dec 25th, 2024
 
   // Check if the current date is before or after the target date
   const isBeforeTarget = currentDate < targetDate;
